870ac5d90fc6232e408516ef6532183fde771773
bezkrovnyyav <42846260+bezkrovnyyav@users.noreply.github.com>
2021-01-08 13:42:50 +0200

    Update Task_1.js

--- Test_task/Task_1.js
+++ Test_task/Task_1.js
@@ -6,7 +6,7 @@ http://prntscr.com/wg5haf
 let x = {"k1":36.9, "k2":37.7, "k3":null, "k4":37.3, "k5":38.1, "k6":36.3}
 
 for (let value in x){
-	if (x[value] === null){
+	if (x[value] == null){
 		console.log(value)
 	}
 }
